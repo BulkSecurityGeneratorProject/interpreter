fd5393ca4a7bd326dfe32ee16beb4dac6e59903d
tfragner <fragner@bitter.at>
2018-01-09 01:18:08 +0100

    parse xml file

--- src/main/java/at/meroff/bac/service/FieldService.java
+++ src/main/java/at/meroff/bac/service/FieldService.java
@@ -1,14 +1,17 @@
 package at.meroff.bac.service;
 
 import at.meroff.bac.domain.Card;
+import at.meroff.bac.domain.Connection;
 import at.meroff.bac.domain.Field;
 import at.meroff.bac.domain.enumeration.CardType;
 import at.meroff.bac.repository.CardRepository;
+import at.meroff.bac.repository.ConnectionRepository;
 import at.meroff.bac.repository.FieldRepository;
 import at.meroff.bac.service.dto.FieldDTO;
 import at.meroff.bac.service.dto.FieldDTOSmall;
 import at.meroff.bac.service.mapper.FieldMapper;
 import at.meroff.bac.service.mapper.FieldSmallMapper;
+import jdk.nashorn.internal.runtime.regexp.joni.constants.NodeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -45,11 +48,14 @@ public class FieldService {
 
     private final FieldSmallMapper fieldSmallMapper;
 
-    public FieldService(FieldRepository fieldRepository, CardRepository cardRepository, FieldMapper fieldMapper, FieldSmallMapper fieldSmallMapper) {
+    private final ConnectionRepository connectionRepository;
+
+    public FieldService(FieldRepository fieldRepository, CardRepository cardRepository, ConnectionRepository connectionRepository, FieldMapper fieldMapper, FieldSmallMapper fieldSmallMapper) {
         this.fieldRepository = fieldRepository;
         this.fieldMapper = fieldMapper;
         this.cardRepository = cardRepository;
         this.fieldSmallMapper = fieldSmallMapper;
+        this.connectionRepository = connectionRepository;
     }
 
     /**
@@ -82,11 +88,11 @@ public class FieldService {
                         Element eElement = (Element) nNode;
 
                         if (eElement.hasAttribute("class") && eElement.getAttribute("class").equals("karterl")) {
-                            String[] coordinates = eElement.getAttribute("d").replaceAll("L","").replaceAll("M", "").split(" ");
-                            coordinates = Arrays.copyOfRange(coordinates,2,10);
+                            String[] coordinates = eElement.getAttribute("d").replaceAll("L", "").replaceAll("M", "").split(" ");
+                            coordinates = Arrays.copyOfRange(coordinates, 2, 10);
                             Card card = new Card();
-                            card.setCardId(Integer.parseInt(((Element)eElement.getParentNode()).getAttribute("id")));
-                            card.setCardType(CardType.values()[Integer.parseInt(((Element)eElement.getParentNode()).getAttribute("id")) % 3]);
+                            card.setCardId(Integer.parseInt(((Element) eElement.getParentNode()).getAttribute("id")));
+                            card.setCardType(CardType.values()[Integer.parseInt(((Element) eElement.getParentNode()).getAttribute("id")) % 3]);
                             card.setx1(Double.parseDouble(coordinates[0]));
                             card.sety1(Double.parseDouble(coordinates[1]));
                             card.setx2(Double.parseDouble(coordinates[2]));
@@ -111,8 +117,91 @@ public class FieldService {
                 e.printStackTrace();
             }
 
-        }
+            try {
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(new ByteArrayInputStream(field.getXmlData()));
+                doc.getDocumentElement().normalize();
 
+                // find all nodes by tag name (cards)
+                NodeList nList = doc.getElementsByTagName("Connection");
+
+                for (int i = 0; i < nList.getLength(); i++) {
+
+                    Connection connection = new Connection();
+
+                    Node nNode = nList.item(i);
+
+                    NodeList childNodes = nNode.getChildNodes();
+
+                    for (int j = 0; j <childNodes.getLength(); j++) {
+                        Node item = childNodes.item(j);
+
+                        if (item.getNodeType() == Node.ELEMENT_NODE) {
+                            System.out.println(item.getNodeName());
+                            System.out.println(item.getNodeType());
+                            if (item.getNodeName().equalsIgnoreCase("uuid")) {
+                                connection.setUuid(Integer.parseInt(item.getTextContent()));
+                            } else if (item.getNodeName().equalsIgnoreCase("name")) {
+                                connection.setName(item.getTextContent());
+                            } else if (item.getNodeName().equalsIgnoreCase("directed1")) {
+                                if (item.getTextContent().equalsIgnoreCase("true")) connection.setDirected1(true); else connection.setDirected1(false);
+                            } else if (item.getNodeName().equalsIgnoreCase("directed2")) {
+                                if (item.getTextContent().equalsIgnoreCase("true")) connection.setDirected2(true); else connection.setDirected2(false);
+                            } else if (item.getNodeName().equalsIgnoreCase("endPoint1")) {
+                                NodeList childNodes1 = item.getChildNodes();
+                                for (int k = 0; k < childNodes1.getLength(); k++) {
+                                    Node point = childNodes1.item(k);
+                                    if (point.getNodeType() == Node.ELEMENT_NODE) {
+                                        if (point.getNodeName().equalsIgnoreCase("uuid")) {
+                                            connection.setEndPoint1Uuid(Integer.parseInt(point.getTextContent()));
+                                        } else if (point.getNodeName().equalsIgnoreCase("x")) {
+                                            connection.setEndPoint1X(Double.parseDouble(point.getTextContent()));
+                                        } else if (point.getNodeName().equalsIgnoreCase("y")) {
+                                            connection.setEndPoint1Y(Double.parseDouble(point.getTextContent()));
+                                        } else if (point.getNodeName().equalsIgnoreCase("angle")) {
+                                            connection.setEndPoint1Angle(Double.parseDouble(point.getTextContent()));
+                                        }
+                                    }
+
+                                }
+                            } else if (item.getNodeName().equalsIgnoreCase("endPoint2")) {
+                                NodeList childNodes1 = item.getChildNodes();
+                                for (int k = 0; k < childNodes1.getLength(); k++) {
+                                    Node point = childNodes1.item(k);
+                                    if (point.getNodeType() == Node.ELEMENT_NODE) {
+                                        if (point.getNodeName().equalsIgnoreCase("uuid")) {
+                                            connection.setEndPoint2Uuid(Integer.parseInt(point.getTextContent()));
+                                        } else if (point.getNodeName().equalsIgnoreCase("x")) {
+                                            connection.setEndPoint2X(Double.parseDouble(point.getTextContent()));
+                                        } else if (point.getNodeName().equalsIgnoreCase("y")) {
+                                            connection.setEndPoint2Y(Double.parseDouble(point.getTextContent()));
+                                        } else if (point.getNodeName().equalsIgnoreCase("angle")) {
+                                            connection.setEndPoint2Angle(Double.parseDouble(point.getTextContent()));
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+
+
+
+                    }
+
+                    connection.setField(field);
+                    connection = connectionRepository.save(connection);
+                    field.addConnection(connection);
+
+
+
+                }
+
+            } catch (ParserConfigurationException | IOException | SAXException e) {
+                e.printStackTrace();
+            }
+
+        }
 
         field = fieldRepository.save(field);
         return fieldMapper.toDto(field);
